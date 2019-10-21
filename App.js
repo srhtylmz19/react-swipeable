f9cf3409459ae196285f641d277b87dc8ba31fe0
srhtylmz19 <srhtylmz19@gmail.com>
2019-10-22 02:36:07 +0400

    modalize

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 
 import React from 'react';
 import List from './src/List';
-import { Text } from 'react-native'
+import CustomStyle from './src/Modalize'
+import { Text, View } from 'react-native'
 
 class App extends React.Component {
   render(){
     return(
-       <List/>
+        <CustomStyle/>
       )
   }
 }
